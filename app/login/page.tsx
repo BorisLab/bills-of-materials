699ee00e2b3959c3ab089871f271dc8830f1497a
BorisLab <alibouakeboris@gmail.com>
2026-07-03 15:12:50 +0200

    fix(css): correction stylisation dashboard admin, formulaire d'inscription

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -63,7 +63,7 @@ export default function LoginPage() {
         </div>
 
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <div className="rounded-md shadow-sm -space-y-px">
+          <div className="rounded-md -space-y-px">
             <div>
               <label htmlFor="email-address" className="sr-only">Email</label>
               <input
